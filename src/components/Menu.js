bbb0c1f0f581534ca098980451b985e9f256c092
Priya Minhas <minhas.priya@gmail.com>
2021-01-14 21:02:22 -0500

    Adding menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Menu extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default Menu;
